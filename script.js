95cbe551efe55d6f44bf0df1a3d0fe22f55457f7
MashP777 <2833225@students.wits.ac.za>
2026-03-02 18:20:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ async function searchCountry(countryName) {
         // Fetch bordering countries
         if (country.borders && country.borders.length>0) {
             const code=country.borders.join(",");
-            const borderRespond=await fetch(`https://restcountries.com/v3.1/alpha/${code}`);
+            const borderRespond=await fetch(`https://restcountries.com/v3.1/alpha?codes=${code}`);
             const borderDataRaw=await borderRespond.json();
             const borderData=Array.isArray(borderDataRaw) ? borderDataRaw: [borderDataRaw];
 
@@ -34,8 +34,10 @@ async function searchCountry(countryName) {
         // Update bordering countries section
         borderCountries.innerHTML=`<h3>Bordering Countries:</h3>`;
         borderData.forEach(neighbor => {
-            borderCountries.innerHTML +=`<p>${neighbor.name.common}</p>
-                <img src="${neighbor.flags.svg}" alt="${neighbor.name.common} flag" width="80">`;
+            const neighborDiv = document.createElement('div');
+            neighborDiv.innerHTML +=`<p>${neighbor.name.common}</p>
+            <img src="${neighbor.flags.svg}" alt="${neighbor.name.common} flag" width="80">`;
+            borderCountries.appendChild(neighborDiv);
         });
     }
     else {
